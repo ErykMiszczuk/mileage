6c75174730990d6110ad66aa27391bf99e21c8da
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2024-02-08 00:01:37 +0100

    Making app more responsive by hiding form when in shown in mobile, and adding a button for displaying modal with new form

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,42 @@ import LineChart from "./components/LineChart";
 import Form from "./components/Form";
 import Table from "./components/Table";
 import RefuelingModal from "./components/RefuelingModal";
-import { Component } from "solid-js";
+import { Component, Show, createSignal } from "solid-js";
 
 const App: Component = () => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const [showForm, setShowForm] = createSignal(true);
+    function handleQueryChange(e: MediaQueryListEvent | MediaQueryList) {
+        if (e.matches) {
+            setShowForm(false);
+        } else {
+            setShowForm(true);
+        }
+    }
+    mediaQuery.addEventListener("change", handleQueryChange);
+    handleQueryChange(mediaQuery);
+
     return (
-        <main class="bg-base-300 h-screen p-2 flex flex-col">
+        <main class="bg-base-300 min-h-screen p-2 flex flex-col">
             <span class="secondary">Mileage</span>
 
-            <div class="flex-1 grid grid-rows-3 lg:grid-cols-4 gap-2">
-                <div class="bg-base-200 p-4 rounded-box">
+            <div class="flex-1 grid grid-rows-2 lg:grid-cols-4 gap-2">
+                <div class="bg-base-200 p-4 rounded-box flex justify-center">
                     <LineChart />
                 </div>
-                <div class="col-span-2 bg-base-200 p-4 rounded-box">
+                <div class="lg:col-span-2 bg-base-200 p-4 rounded-box">
                     <Table />
                 </div>
-                <div class="bg-base-200 p-4 rounded-box">
-                    <Form />
-                </div>
+                <Show when={showForm()}>
+                    <div class="bg-base-200 p-4 rounded-box">
+                        <Form />
+                    </div>
+                </Show>
             </div>
 
-            <RefuelingModal />
+            <Show when={!showForm()}>
+                <RefuelingModal />
+            </Show>
         </main>
     );
 };
